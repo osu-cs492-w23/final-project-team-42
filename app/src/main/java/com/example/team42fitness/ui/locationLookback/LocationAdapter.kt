efb794dff780f60494cb96ee98382e73785c07ad
Angelo Matteo <angmatt90@gmail.com>
2023-03-22 15:54:00 -0700

    recyclerview hooked up with fitness database

--- app/src/main/java/com/example/team42fitness/ui/locationLookback/LocationAdapter.kt
+++ app/src/main/java/com/example/team42fitness/ui/locationLookback/LocationAdapter.kt
@@ -1,23 +1,28 @@
 package com.example.team42fitness.ui.locationLookback
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.team42fitness.R
+import com.example.team42fitness.data.activity.FitnessActivity
 import com.example.team42fitness.data.locationLookback.LocationDate
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * For use with LocationViewModel, which is used in main screen for location entry feature
  */
 class LocationAdapter(private val onClick: (LocationDate) -> Unit) : RecyclerView.Adapter<LocationAdapter.ViewHolder>()
 {
-    private val locationDates: MutableList<LocationDate> = mutableListOf()
+    private var fitnessActivities: List<FitnessActivity> = mutableListOf()
 
 
-
-    override fun getItemCount() = this.locationDates.size
+    override fun getItemCount() = this.fitnessActivities.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
     {
@@ -27,37 +32,51 @@ class LocationAdapter(private val onClick: (LocationDate) -> Unit) : RecyclerVie
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int)
     {
-        holder.bind(this.locationDates[position])
+        holder.bind(this.fitnessActivities[position])
     }
 
-
-    /**
-     * For adding a day to the list in the main screen for location entry feature. Currently pre-populating list.
-     */
-    fun addDate(locationDate: LocationDate)
-    {
-        locationDates.add(locationDate)
+    fun addFitnessActivities(fitnessActivities: List<FitnessActivity>) {
+        this.fitnessActivities = fitnessActivities
         notifyDataSetChanged()
-        // notifyItemChanged(0)
     }
 
 
     class ViewHolder(itemView: View, val onClick: (LocationDate) -> Unit) : RecyclerView.ViewHolder(itemView)
     {
-        private var specifiedDate: LocationDate? = null
+        private var fitnessActivity: FitnessActivity? = null
         private val dateTV = itemView.findViewById<TextView>(R.id.tv_location_date_text)
-
+        private val stepsTV = itemView.findViewById<TextView>(R.id.tv_steps)
 
         init
         {
-            itemView.setOnClickListener { specifiedDate?.let(onClick) }
+            itemView.setOnClickListener {
+                if (fitnessActivity != null) {
+                    val uriBase = "geo:0,0?q="
+                    val uri: String = uriBase + fitnessActivity!!.location
+
+                    val geoUri = Uri.parse(uri)
+
+                    val intent = Intent(Intent.ACTION_VIEW, geoUri)
+
+                    try {
+
+                        itemView.context.startActivity(intent)
+                    } catch (e: ActivityNotFoundException) {
+                        Snackbar.make(
+                            itemView,
+                            "An app wasn't found to be able to open and display the location for the entry you entered... :(",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
         }
 
-        fun bind(locationDate: LocationDate)
+        fun bind(fitnessActivity: FitnessActivity)
         {
-            specifiedDate = locationDate
-            dateTV.text = locationDate.date
-
+            this.fitnessActivity = fitnessActivity
+            dateTV.text = fitnessActivity.date
+            stepsTV.text = "Steps: " + fitnessActivity.steps.toString()
         }
     }
 }
